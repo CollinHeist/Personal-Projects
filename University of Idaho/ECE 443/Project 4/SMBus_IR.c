2d06fc59dc8791e4263564b9726e4f4e49437d76
Collin Heist <heis0741@vandals.uidaho.edu>
2019-10-07 20:26:37 -0700

    Added Doxygen to SMBus Files

--- University of Idaho/ECE 443/Project 4/SMBus_IR.c
+++ University of Idaho/ECE 443/Project 4/SMBus_IR.c
@@ -1,11 +1,17 @@
+/** 
+ *	@file 	SMBus_IR.c
+ *	@brief	Library file for interfacing with the IR Sensor over SMBus
+ *	@author	Collin Heist
+ **/
+
+// File Inclusion
 #include <plib.h>
 #include "chipKIT_Pro_MX7.h"
 #include "SMBus_IR.h"
 
 /**	
- *	@brief  Initialize the hardware necessary for the IR Sensor (I2C1).
- *	@param  None.
- *	@return None.
+ *	@brief	Initialize the hardware necessary for the IR Sensor (I2C1).
+ *	@return	None.
  **/
 void initialize_ir_sensor(void) {
 	OpenI2C1(I2C_EN, I2C_CLOCK_VAL);	// I2C_EN or I2C_SM_EN
@@ -13,14 +19,13 @@ void initialize_ir_sensor(void) {
 }
 
 /**	
- *	@brief  Read the temperature of the IR Sensor.
- *	@param  None.
- *	@return (Float) Temperature listed in T_OBJ_ADDR of IR sensor, after conversion to Celsius.
+ *	@brief	Read the temperature of the IR Sensor.
+ *	@return	(Float) Temperature listed in T_OBJ_ADDR of IR sensor, after conversion to Celsius.
  **/
 float read_ir_temp(void) {
-	int i2c_data[3] = {0};						/// Empty array that the I2C data will be read into
+	int i2c_data[3] = {0};						// Empty array that the I2C data will be read into
 	
-	/// Start an I2C transaction
+	// Start an I2C transaction
 	StartI2C1();
 	IdleI2C1();
 	
@@ -30,18 +35,18 @@ float read_ir_temp(void) {
 	 **/
 	wait_for_ack(SLAVE_ADDR << 1 | WRITE);
 	wait_for_ack(T_OBJ_ADDR);
-	RestartI2C1();								/// Send a ReSTART
+	RestartI2C1();								// Send a ReSTART
 	IdleI2C1();
 	wait_for_ack(SLAVE_ADDR << 1 | READ);
 	
-	int length = 3;								/// We'll be reading three I2C bytes
+	int length = 3;								// We'll be reading three I2C bytes
 	int index = 0;
 	while (length--) {
-		i2c_data[index++] = MasterReadI2C1();	/// Read the incoming byte
-		AckI2C1();								/// ACK the received byte
+		i2c_data[index++] = MasterReadI2C1();	// Read the incoming byte
+		AckI2C1();								// ACK the received byte
 		IdleI2C1();
 	}
-	StopI2C1();									/// Terminate the read with a STOP
+	StopI2C1();									// Terminate the read with a STOP
 	IdleI2C1();
 	
 	/**
@@ -49,7 +54,7 @@ float read_ir_temp(void) {
 	 *	Check if the most significant byte is high, indicating an error
 	 **/
 	unsigned int raw_temp = i2c_data[0] + (i2c_data[1] << 8);
-	if (raw_temp & READ_ERROR_FLAG)				/// The error flag is high, return accordingly
+	if (raw_temp & READ_ERROR_FLAG)				// The error flag is high, return accordingly
 		return ERROR_TEMP;
 	
 	/// Return the temperature in degC
@@ -57,9 +62,9 @@ float read_ir_temp(void) {
 }
 
 /**	
- *	@brief  Write to I2C1, resending the data until an ACK is received 
- *	@param  (unsigned int) The I2C data byte being sent to I2C1.
- *	@return None.
+ *	@brief	Write to I2C1, resending the data until an ACK is received 
+ *	@param	data_bye	The I2C data byte being sent to I2C1.
+ *	@return	None.
  **/
 static void wait_for_ack(unsigned int data_byte) {
 	/// Resend the data byte until an acknowledge is received
@@ -68,7 +73,7 @@ static void wait_for_ack(unsigned int data_byte) {
 		MasterWriteI2C1(data_byte);
 		IdleI2C1();
 		
-		/// ACKSTAT is 0 when the slave acknowledges 
+		// ACKSTAT is 0 when the slave acknowledges 
 		if (!I2C1STATbits.ACKSTAT)
 			break;
 	}
--- University of Idaho/ECE 443/Project 4/SMBus_IR.h
+++ University of Idaho/ECE 443/Project 4/SMBus_IR.h
@@ -1,24 +1,30 @@
+/**
+ *	@file	SMBUS_IR.h
+ *	@brief	Header for the IR-sensor library
+ *	@author	Collin Heist 
+ */
+
 #ifndef __SMBUS_IR_H__
 	#define __SMBUS_IR_H__
 
-	/// I2C Configuration Settings
-	#define FSCK					40000
+	// I2C Configuration Settings
+	#define FSCK					40000					//!< The clock setup for the I2C peripheral
 	#define I2C_CLOCK_VAL			((FPB / 2 / FSCK) - 2)
-	#define ATTEMPT_COUNT			10
+	#define ATTEMPT_COUNT			10						//!< How many attempts
 
-	/// Settings for interfacing with the IR Sensor
-	#define SLAVE_ADDR				0x005A					/// Default slave address of the IR Sensor
-	#define	T_OBJ_ADDR				0x0007					/// Address in RAM of the sensor's read temperature
-	#define READ_ERROR_FLAG			0x8000					/// This bit corresponds to the IR sensor's error flag
-	#define ERROR_TEMP				(-1000.0)
-	#define IR_SENSOR_RES			0.02					/// Each bit corresponds to 0.02 degC
-	#define KELVIN_TO_CELSIUS(C)	((float) C - 273.15)
+	// Settings for interfacing with the IR Sensor
+	#define SLAVE_ADDR				0x005A					//!< Default slave address of the IR Sensor
+	#define	T_OBJ_ADDR				0x0007					//!< Address in RAM of the sensor's read temperature
+	#define READ_ERROR_FLAG			0x8000					//!< This bit corresponds to the IR sensor's error flag
+	#define ERROR_TEMP				(-1000.0)				//!< Value used to return an error read
+	#define IR_SENSOR_RES			0.02					//!< Each bit corresponds to 0.02 degC
+	#define KELVIN_TO_CELSIUS(C)	((float) C - 273.15)	//!< Macro to convert a temperature from C->K
 
-	/// I2C bit levels - write is LOW, read is HIGH
+	// I2C bit levels - write is LOW, read is HIGH
 	#define WRITE					0
 	#define READ					1	
 
-	/// Function Prototypes
+	// Function Prototypes
 	void initialize_ir_sensor(void);
 	float read_ir_temp(void);
 	static void wait_for_ack(unsigned int data_byte);
